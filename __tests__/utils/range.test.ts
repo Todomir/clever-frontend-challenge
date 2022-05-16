5d1edfd85c21211c840c7ceb2f481c46c211b84f
Abner Rodrigues <abnerluis1001@gmail.com>
2022-05-16 08:52:49 -0300

    :ambulance: quickfix(utils/range): remove wrongly implemented error message

--- __tests__/utils/range.test.ts
+++ __tests__/utils/range.test.ts
@@ -6,8 +6,4 @@ describe('utils/range', () => {
   it('should return an array of numbers', () => {
     expect(range(1, 5)).toEqual([1, 2, 3, 4, 5]);
   });
-
-  it('should throw an error if the start is greater than the end', () => {
-    expect(() => range(5, 1)).toThrowError();
-  });
 });
